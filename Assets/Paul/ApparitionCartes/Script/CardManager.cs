2a75177c9e1cc2b8608ba6e15146435443b9ae5f
Paul Leduc <paulleduc72@gmail.com>
2020-04-08 12:11:28 +0200

    Crad manager effets différenciers

--- Assets/Paul/ApparitionCartes/Script/CardManager.cs
+++ Assets/Paul/ApparitionCartes/Script/CardManager.cs
@@ -2,20 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class CardManager : MonoBehaviour
 {
     [HideInInspector] public Text _titreText, _descriptionText;
-    int _actualEffect;
-    int _numberOfUsing;
+
+    [HideInInspector] public ScriptableEffect _scriptableEffect;
+
+    [HideInInspector] public bool _goUp;
+    [HideInInspector] public bool _goDown;
+    int _speed;
+
+    [HideInInspector] public GameObject _otherCard01, _otherCard02;
+
+    string _numberCard;
 
     void Start()
     {
-        AttribuerEffect();
+        _numberCard = (Regex.Replace(this.name, "[^0-9]", ""));
+        _speed = FindObjectOfType<CardManagerGlobal>()._speedReveals;
+    }
+    public void AttribuerEffect()
+    {
+        _titreText.text = _scriptableEffect._effectTitle;
+        _descriptionText.text = _scriptableEffect._effectDescription;
     }
 
-    void AttribuerEffect()
+    void Update()
     {
+        if (_goUp)
+            GoUp();
+        if (_goDown)
+            GoDown();
+    }
+
+    public void GoUp()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + _speed);
+        if (transform.position.y >= 540)
+        {
+            _goUp = false;
+        }
+    }
+    public void GoDown()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y - _speed);
+        if (transform.position.y <= -540)
+        {
+            _goDown = false;
+        }
+    }
+
+    public void SelectionnerUneCarte()
+    {
+        if (FindObjectOfType<CardManagerGlobal>()._cardSelected == "")
+        {
+            FindObjectOfType<CardManagerGlobal>()._cardSelected = _numberCard;
+            Debug.Log(_numberCard);
+            Debug.Log(FindObjectOfType<CardManagerGlobal>()._cardSelected);
+
+            if (FindObjectOfType<CardManagerGlobal>()._cardSelected == _numberCard)
+            {
+                StartCoroutine(ThisCardSelected());
+                _otherCard01.GetComponent<CardManager>()._goDown = true;
+                _otherCard02.GetComponent<CardManager>()._goDown = true;
+            }
+        }
+    }
+
+    IEnumerator ThisCardSelected()
+    {
+        Debug.Log(_scriptableEffect._effectTitle);
+        yield return new WaitForSeconds(FindObjectOfType<CardManagerGlobal>()._timeCardSelected);
+        _goDown = true;
 
     }
 }
---
+++ Assets/Paul/ApparitionCartes/Script/CardManagerGlobal.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManagerGlobal : MonoBehaviour
+{
+
+    [HideInInspector] public string _cardSelected;
+    [Header("Déposez vos scriptables objects là(augmentez la size)")]
+    public List<ScriptableEffect> _scriptableEffects;
+    [HideInInspector] public List<CardAlreadyUsed> _cardAlreadyUsed;
+
+    CardManager _card01, _card02, _card03;
+    int _tirage01, _tirage02, _tirage03;
+    [Header("Apparition des cartes")]
+    public int _speedReveals;
+    [Range(0f,1f)]
+    public float _timeToWaitBeforeNewCardAppears;
+    public float _timeCardSelected;
+
+    void Start()
+    {
+        _card01 = GameObject.Find("Card01").GetComponent<CardManager>();
+        _card02 = GameObject.Find("Card02").GetComponent<CardManager>();
+        _card03 = GameObject.Find("Card03").GetComponent<CardManager>();
+
+        for (int i= 0; i<_scriptableEffects.Count; i++)
+        {
+            _cardAlreadyUsed.Add(new CardAlreadyUsed(_scriptableEffects[i]._effectTitle, _scriptableEffects[i]._numberOfReusing, _scriptableEffects[i]._isInfinite));
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AttributeCard01();
+        }
+    }
+
+    public void AttributeCard01()
+    {
+        if (_cardAlreadyUsed.Count > 0)
+        {
+            _tirage01 = Random.Range(0, _cardAlreadyUsed.Count);
+            Debug.Log(_tirage01);
+
+            _card01._scriptableEffect = _scriptableEffects[_tirage01];
+            _card01.AttribuerEffect();
+            _card01._goUp = true;
+
+            //Si la carte est infinie
+            if (!_cardAlreadyUsed[_tirage01]._isInfinite)
+            {
+                _cardAlreadyUsed[_tirage01]._numberOfUsing--;
+            }
+            //Retirer la carte si elle atteint 0
+            if (_cardAlreadyUsed[_tirage01]._numberOfUsing <= 0)
+            {
+                _cardAlreadyUsed.Remove(_cardAlreadyUsed[_tirage01]);
+            }
+            StartCoroutine(AttributeCard02());
+        }
+        
+    }
+    IEnumerator AttributeCard02()
+    {
+        if (_cardAlreadyUsed.Count > 0)
+        {
+            _tirage02 = Random.Range(0, _cardAlreadyUsed.Count);
+            if (_tirage02 == _tirage01)
+            {
+                StartCoroutine(AttributeCard02());
+            }
+            else
+            {
+                Debug.Log(_tirage02);
+                yield return new WaitForSeconds(_timeToWaitBeforeNewCardAppears);
+                _card02._scriptableEffect = _scriptableEffects[_tirage02];
+                _card02.AttribuerEffect();
+                _card02._goUp = true;
+
+                //Si la carte est infinie
+                if (!_cardAlreadyUsed[_tirage02]._isInfinite)
+                {
+                    _cardAlreadyUsed[_tirage02]._numberOfUsing--;
+                }
+                //Retirer la carte si elle atteint 0
+                if (_cardAlreadyUsed[_tirage02]._numberOfUsing <= 0)
+                {
+                    _cardAlreadyUsed.Remove(_cardAlreadyUsed[_tirage02]);
+                }
+
+                StartCoroutine(AttributeCard03());
+            }
+        }
+
+            
+    }
+
+    IEnumerator AttributeCard03()
+    {
+        if (_cardAlreadyUsed.Count > 0)
+        {
+            _tirage03 = Random.Range(0, _cardAlreadyUsed.Count);
+            if (_tirage03 == _tirage01 || _tirage03 == _tirage02)
+            {
+                StartCoroutine(AttributeCard03());
+            }
+            else
+            {
+                Debug.Log(_tirage03);
+                yield return new WaitForSeconds(_timeToWaitBeforeNewCardAppears);
+                _card03._scriptableEffect = _scriptableEffects[_tirage03];
+                _card03.AttribuerEffect();
+                _card03._goUp = true;
+
+                //Si la carte est infinie
+                if (!_cardAlreadyUsed[_tirage03]._isInfinite)
+                {
+                    _cardAlreadyUsed[_tirage03]._numberOfUsing--;
+                }
+                //Retirer la carte si elle atteint 0
+                if (_cardAlreadyUsed[_tirage03]._numberOfUsing <= 0)
+                {
+                    _cardAlreadyUsed.Remove(_cardAlreadyUsed[_tirage03]);
+                }
+            }
+
+               
+        }
+    }
+}
