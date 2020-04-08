13a57e29a0f3be2e8e63d741e2f2ac205dfb78e9
Paul Leduc <paulleduc72@gmail.com>
2020-04-08 09:54:19 +0200

    Collision ennemies light

---
+++ Assets/Paul/ApparitionCartes/CardManagerGlobal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManagerGlobal : MonoBehaviour
+{
+    public List<ScriptableEffect> _scriptableEffects;
+    public List<CardAlreadyUsed> _cardAlreadyUsed;
+
+    public CardManager _card01;
+    int _tirage01;
+
+    void Start()
+    {
+        for (int i= 0; i<_scriptableEffects.Count; i++)
+        {
+            Debug.Log(i);
+            _cardAlreadyUsed.Add(new CardAlreadyUsed(_scriptableEffects[i]._effectTitle, _scriptableEffects[i]._numberOfReusing));
+        }
+    }
+
+    void AttributeCard()
+    {
+        _tirage01 = Random.Range(0, _scriptableEffects.Count - 1);
+        //if(_cardAlreadyUsed)
+    }
+}
