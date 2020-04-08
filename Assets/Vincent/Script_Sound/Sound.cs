7dcd96ec59d4a75e3f89c8d17fa7aa06f6051513
Vincent <vincent_1999@hotmail.fr>
2020-04-08 18:09:46 +0200

    Ajout module de son avec editeur pour les GD, Readme pour comment utiliser

---
+++ Assets/Vincent/Script_Sound/Sound.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public enum TYPE_AUDIO
+{
+    MusiqueMenuDemarrageDuJeu,
+    MusiqueQuandOnCliqueSurUnBouton,
+    BruitsAmbiance,
+    BruitDesMonstres,
+    RugissementMonstre1,
+    RugissementMonstre2,
+    RugissementMonstre3,
+    RugissementMonstre4,
+    RugissementMonstre5,
+    BruitOmbre,
+    ParquetQuiGrince,
+    GrelottementPeurEnfant,
+    GemissementEnfant,
+    Horloge,
+    ChangementDuModeLampe,
+    ClicLampeOnOff,
+    BruitQuandLaLampeEstDecharger,
+    BruitVieuxBoisMeubleQuiGrincent,
+    BruitAllumageProjecteur,
+    BruitMortMonstreEtOmbre,
+    BruitAttaqueMonstre,
+    BruitMortEnfant,
+    Defaite,
+    ChangementDeJour,
+    ClacDuPiegeActiver,
+    BruitDePreactivationPiege,
+    BruitMonstreToucher,
+    BruitExplosionMonstre
+}
+
+[System.Serializable]
+public class Sound
+{
+    public TYPE_AUDIO audioFor;
+    public AudioClip audio;
+    public bool loop;
+    public bool playOnAwake;
+}
---
+++ Assets/Vincent/Script_Sound/testCubeEventPlaySong.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayOneSound))]
+public class testCubeEventPlaySong : MonoBehaviour
+{
+
+    PlayOneSound playSound;
+
+    public bool activateSound = false;
+
+    private void Start()
+    {
+        playSound = GetComponent<PlayOneSound>();
+    }
+
+    void Update()
+    {
+        if (activateSound == true)
+        {
+            playSound.PlaySound();
+            activateSound = false;
+        }
+    }
+
+}
