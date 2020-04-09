77e9643c2f0e059d117b26104b7b228647304f94
Theo Driutti <theo.driutti57@gmail.com>
2020-04-09 14:50:58 +0200

    Fix conflict

--- Assets/Paul/Flashlight/SphereCaster.cs
+++ Assets/Paul/Flashlight/SphereCaster.cs
@@ -5,50 +5,21 @@ using UnityEngine;
 public class SphereCaster : MonoBehaviour
 {
     public float _sphereRadius, _maxDistance;
-<<<<<<< HEAD
-    public LayerMask _layerMask;
-
-    public GameObject _currentHitObject;
-=======
     public LayerMask _layerEnemy;
     public LayerMask _layerTrap;
->>>>>>> fbefcaf89676c4bafc6ec5ed081e435607ba2fd7
 
     Vector3 _origin, _direction;
 
     float _currentHitDistance;
 
     public List<GameObject> _enemyTouched;
-<<<<<<< HEAD
-=======
     public List<GameObject> _trapTouched;
->>>>>>> fbefcaf89676c4bafc6ec5ed081e435607ba2fd7
 
     void Update()
     {
         _origin = transform.position;
         _direction = transform.forward;
 
-<<<<<<< HEAD
-        RaycastHit _hit;
-        if (Physics.SphereCast(_origin, _sphereRadius, _direction, out _hit, _maxDistance, _layerMask, QueryTriggerInteraction.UseGlobal))
-        {
-            if (!_enemyTouched.Contains(_currentHitObject))
-            {
-                _enemyTouched.Add(_currentHitObject);
-            }
-            _currentHitObject = _hit.transform.gameObject;
-            _currentHitDistance = _hit.distance;
-        }
-        else
-        {
-            if (_enemyTouched.Contains(_currentHitObject))
-            {
-                _enemyTouched.Remove(_currentHitObject);
-            }
-            _currentHitDistance = _maxDistance;
-            _currentHitObject = null;
-=======
         _currentHitDistance = _maxDistance;
         _enemyTouched.Clear();
         RaycastHit[] _hitsEnemy = Physics.SphereCastAll(_origin, _sphereRadius, _direction, _maxDistance, _layerEnemy, QueryTriggerInteraction.UseGlobal);
@@ -63,7 +34,6 @@ public class SphereCaster : MonoBehaviour
         {
             _trapTouched.Add(hit.transform.gameObject);
             _currentHitDistance = hit.distance;
->>>>>>> fbefcaf89676c4bafc6ec5ed081e435607ba2fd7
         }
     }
 
